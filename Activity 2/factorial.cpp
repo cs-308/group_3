afd452f8cb829082e5b3515c48d7e5ba8c560ec7
Pushpendra  Choudhary <pushpendrachoudhary@Pushpendras-MacBook-Air.local>
2022-08-29 15:45:42 +0530

    changed for 2nd time

--- Activity 2/factorial.cpp
+++ Activity 2/factorial.cpp
@@ -2,10 +2,14 @@
 #include "functions.h"
 using namespace std;
 int factorial(int n){
+    int i=0;
+    i++;
     if(n<0){
         cout<<"Renter a number ,only positive numbers are allowed."<<endl;
     }
     else if(n!=1){
+        cout<<"Recursive function called for: "<<endl;
+        
 	return(n * factorial(n-1));
     }
     else return 1;
