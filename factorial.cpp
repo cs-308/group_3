b0fe79aaa26ac9bc6146f715961ad510f3218b34
Nikhil <dnikhil.1606@gmail.com>
2022-09-05 15:16:55 +0000

    changed cout to printf

--- factorial.cpp
+++ factorial.cpp
@@ -5,10 +5,10 @@ int factorial(int n){
     int i=0;
     i++;
     if(n<0){
-        cout<<"Renter a number ,only positive numbers are allowed."<<endl;
+        printf("Renter a number ,only positive numbers are allowed.");
     }
     else if(n!=1){
-        cout<<"Recursive function called for: "<<i<<endl;
+        printf("Recursive function called for: ",i);
         
 	return(n * factorial(n-1));
     }
